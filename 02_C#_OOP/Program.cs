bbcf54dc00ff2d0118a251fd12f4cdd064fe02c7
Enamul Hasan <syl.enamul@gmail.com>
2025-01-10 22:16:12 +0600

    code: Added Constructor, class

---
+++ 02_C#_OOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Fundamentals;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Intro();
+
+            var max = new Person("Max");
+            max.LastName = "Smith";
+            max.Intro();
+
+            var order = new Order();
+            var customer = new Customer("Max");
+            customer.Orders.Add(order);
+            customer.Intro();
+
+    }
+}
